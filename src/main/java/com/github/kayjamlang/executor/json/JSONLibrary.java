3b27bf0e5112407914c82407cbfdec430deab6de
levkopo <40952805+levkopo@users.noreply.github.com>
2021-05-02 13:59:44 +0500

    update to 0.1.3.20

--- src/main/java/com/github/kayjamlang/executor/json/JSONLibrary.java
+++ src/main/java/com/github/kayjamlang/executor/json/JSONLibrary.java
@@ -1,7 +1,8 @@
 package com.github.kayjamlang.executor.json;
 
-import com.github.kayjamlang.core.Argument;
+import com.github.kayjamlang.core.expressions.data.Argument;
 import com.github.kayjamlang.core.Type;
+import com.github.kayjamlang.executor.Context;
 import com.github.kayjamlang.executor.Executor;
 import com.github.kayjamlang.executor.libs.Library;
 import com.github.kayjamlang.executor.libs.main.ArrayClass;
@@ -18,17 +19,6 @@ public class JSONLibrary extends Library {
 
     public JSONLibrary() throws Exception {
         classes.put("JSON", new JSONClass());
-        addFunction(new LibFunction("jsonDecode", (mainContext, context) -> {
-            String jsonEncoded = (String) context.variables.get("jsonEncoded");
-            try {
-                if(jsonEncoded.startsWith("{"))
-                    return decodeObject(mainContext.executor, new JSONObject(jsonEncoded));
-                return decodeArray(mainContext.executor, new JSONArray(jsonEncoded));
-            }catch (Exception e){
-                e.printStackTrace();
-                return false;
-            }
-        }, new Argument(Type.STRING, "jsonEncoded")));
     }
 
     public Object decode(Executor executor, Object value) throws Exception {
@@ -40,6 +30,41 @@ public class JSONLibrary extends Library {
             return ((Number) value).longValue();
 
         return value;
+    }
+
+    public Object encodeMap(Executor executor, MapClass object) throws Exception {
+        Map<Object, Object> map = object.getMap((Context) object.data.get("ctx"));
+
+        JSONObject jsonObject = new JSONObject();
+        for(Map.Entry<Object, Object> entry: map.entrySet()) {
+            Object value = entry.getValue();
+            if(value instanceof MapClass)
+                value = encodeMap(executor, (MapClass) value);
+            else if(value instanceof ArrayClass)
+                value = encodeArray(executor, (ArrayClass) value);
+
+            jsonObject.put(entry.getKey().toString(), value);
+        }
+
+
+        return jsonObject;
+    }
+
+    public Object encodeArray(Executor executor, ArrayClass object) throws Exception {
+        List<Object> array = object.getVariable((Context) object.data.get("ctx"), "array");
+
+        JSONArray jsonArray = new JSONArray();
+        for(Object value: array) {
+            if(value instanceof MapClass)
+                value = encodeMap(executor, (MapClass) value);
+            else if(value instanceof ArrayClass)
+                value = encodeArray(executor, (ArrayClass) value);
+
+            jsonArray.put(value);
+        }
+
+
+        return jsonArray;
     }
 
     public Object decodeObject(Executor executor, JSONObject object) throws Exception {
@@ -64,8 +89,18 @@ public class JSONLibrary extends Library {
         public JSONClass() throws Exception {
             super("JSON", null);
             setCompanion(new LibObject((object -> {
-                object.addFunction(new LibFunction("decode", (mainContext, context) -> {
-                    String jsonEncoded = (String) context.variables.get("jsonEncoded");
+                object.addFunction(new LibFunction("encode", Type.STRING, (mainContext, context) -> {
+                    Object value = context.getVariable("jsonEncoded");
+                    if(value instanceof ArrayClass)
+                        return encodeArray(mainContext.executor, (ArrayClass) value);
+                    else if(value instanceof MapClass)
+                        return encodeMap(mainContext.executor, (MapClass) value);
+
+                    return false;
+                }, new Argument(Type.ANY, "jsonEncoded")));
+
+                object.addFunction(new LibFunction("decode", Type.ANY, (mainContext, context) -> {
+                    String jsonEncoded = context.getVariable("jsonEncoded");
                     try {
                         if(jsonEncoded.startsWith("{"))
                             return decodeObject(mainContext.executor, new JSONObject(jsonEncoded));
